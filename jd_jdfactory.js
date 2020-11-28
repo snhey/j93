33de372681953336ef590163757db12bb70bbf49
269569205 <269569205@qq.com>
2020-11-28 20:22:48 +0800

    Update jd_jdfactory.js

--- jd_jdfactory.js
+++ jd_jdfactory.js
@@ -588,6 +588,7 @@ function requireConfig() {
     console.log(`开始获取${$.name}配置文件\n`);
     //Node.js用户请在jdCookie.js处填写京东ck;
     const shareCodes = $.isNode() ? require('./jdFactoryShareCodes.js') : '';
+    console.log(shareCodes);
     console.log(`共${cookiesArr.length}个京东账号\n`);
     $.shareCodesArr = [];
     if ($.isNode()) {
