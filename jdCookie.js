3694b0a0b0793fac56ca014976e62bd15278f566
269569205 <269569205@qq.com>
2020-12-22 09:10:55 +0800

    Update jdCookie.js

--- jdCookie.js
+++ jdCookie.js
@@ -4,6 +4,7 @@
 //此处填写京东账号cookie。
 //注：github action用户cookie填写到Settings-Secrets里面，新增JD_COOKIE，多个账号的cookie使用`&`隔开或者换行
 let CookieJDs = [
+''
  ]
 // 判断github action里面是否有京东ck
 if (process.env.JD_COOKIE) {
