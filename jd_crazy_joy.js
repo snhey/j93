5ca3cfe4d99a18e2267b70b345f5f86c035b7e3f
269569205 <269569205@qq.com>
2020-12-25 15:46:15 +0800

    Update jd_crazy_joy.js

--- jd_crazy_joy.js
+++ jd_crazy_joy.js
@@ -29,7 +29,7 @@ const JD_API_HOST = 'https://api.m.jd.com/';
 
 const notify = $.isNode() ? require('./sendNotify') : '';
 let jdNotify = true;//是否关闭通知，false打开通知推送，true关闭通知推送
-let helpSelf = false // 循环助力，默认关闭
+let helpSelf = true// 循环助力，默认关闭
 let applyJdBean = 0
 let cookiesArr = [], cookie = '', message = '';
 const inviteCodes = [
