92827f05e306a3da02758e196ab4a324e6b77c0e
admin <269569205@163.com>
2021-01-20 20:09:58 +0800

    nian

--- jd_superMarket.js
+++ jd_superMarket.js
@@ -20,6 +20,7 @@ cron "11 1-23/5 * * *" script-path=https://raw.githubusercontent.com/LXK9301/jd_
 // Surge
 东东超市 = type=cron,cronexp="11 1-23/5 * * *",wake-system=1,timeout=320,script-path=https://raw.githubusercontent.com/LXK9301/jd_scripts/master/jd_superMarket.js
  */
+console.log("sssssssssss-----------------------")
 const $ = new Env('东东超市');
 //Node.js用户请在jdCookie.js处填写京东ck;
 //IOS等用户直接用NobyDa的jd cookie
